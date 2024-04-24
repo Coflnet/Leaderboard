c5c845ebb6c7b89ed0799ef70e3b981cdeb5b73e
Äkwav <ekwav@coflnet.com>
2024-04-24 11:55:38 +0200

    use .net 6 client

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,15 +1,18 @@
-VERSION=0.2.1
+VERSION=0.2.2
 
 rm -r out
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5042/api/swagger/v1/swagger.json \
 -g csharp \
--o /local/out --additional-properties=packageName=Coflnet.Leaderboard.Client,packageVersion=$VERSION,licenseId=MIT
+-o /local/out --additional-properties=packageName=Coflnet.Leaderboard.Client,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
 
 cd out
-sed -i 's/GIT_USER_ID/Coflnet/g' src/Coflnet.Leaderboard.Client/Coflnet.Leaderboard.Client.csproj
-sed -i 's/GIT_REPO_ID/Leaderboard/g' src/Coflnet.Leaderboard.Client/Coflnet.Leaderboard.Client.csproj
-sed -i 's/>OpenAPI/>Coflnet/g' src/Coflnet.Leaderboard.Client/Coflnet.Leaderboard.Client.csproj
+path=src/Coflnet.Leaderboard.Client/Coflnet.Leaderboard.Client.csproj
+sed -i 's/GIT_USER_ID/Coflnet/g' $path
+sed -i 's/GIT_REPO_ID/Leaderboard/g' $path
+sed -i 's/>OpenAPI/>Coflnet/g' $path
+sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>README.md</PackageReadmeFile>@g' $path
+sed -i 's@Remove="System.Web" />@Remove="System.Web" />\n    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>@g' $path
 
 dotnet pack
-cp src/Coflnet.Leaderboard.Client/bin/Debug/Coflnet.Leaderboard.Client.*.nupkg ..
+cp src/Coflnet.Leaderboard.Client/bin/Release/Coflnet.Leaderboard.Client.*.nupkg ..
