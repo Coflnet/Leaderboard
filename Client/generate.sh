5a83fe235a3ef28a2047f644d36aff8de868899a
Äkwav <ekwav@coflnet.com>
2024-11-07 12:06:11 +0100

    update packages

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,18 +1,20 @@
-VERSION=0.3.0
+VERSION=0.3.1
+PACKAGE_NAME=Coflnet.Leaderboard.Client
 
 rm -r out
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5042/api/swagger/v1/swagger.json \
 -g csharp \
--o /local/out --additional-properties=packageName=Coflnet.Leaderboard.Client,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
+-o /local/out --additional-properties=packageName=$PACKAGE_NAME,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
 
 cd out
-path=src/Coflnet.Leaderboard.Client/Coflnet.Leaderboard.Client.csproj
+path=src/$PACKAGE_NAME/$PACKAGE_NAME.csproj
 sed -i 's/GIT_USER_ID/Coflnet/g' $path
 sed -i 's/GIT_REPO_ID/Leaderboard/g' $path
 sed -i 's/>OpenAPI/>Coflnet/g' $path
 sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>README.md</PackageReadmeFile>@g' $path
-sed -i 's@Remove="System.Web" />@Remove="System.Web" />\n    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>@g' $path
+sed -i '34i    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>' $path
 
 dotnet pack
-cp src/Coflnet.Leaderboard.Client/bin/Release/Coflnet.Leaderboard.Client.*.nupkg ..
+cp src/$PACKAGE_NAME/bin/Release/$PACKAGE_NAME.*.nupkg ..
+dotnet nuget push ../$PACKAGE_NAME.$VERSION.nupkg --api-key $NUGET_API_KEY --source "nuget.org" --skip-duplicate
